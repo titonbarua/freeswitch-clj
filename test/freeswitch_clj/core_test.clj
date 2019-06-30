d639ececac0ba11349817fa493c76442439434ab
Titon Barua <titon@vimmaniac.com>
2019-06-30 14:18:15 +0600

    Test is now configurable with FS_HOST, FST_PORT and FS_PASS environment variables.

--- test/freeswitch_clj/core_test.clj
+++ test/freeswitch_clj/core_test.clj
@@ -3,75 +3,79 @@
             [freeswitch-clj.core :refer :all]))
 
 (deftest test-fs-inbound-session
-  (let [conn (connect :host "127.0.0.1"
-                      :port 8021
-                      :password "ClueCon")]
+  (let [env     (System/getenv)
+        fs-host (get env "FS_HOST" "127.0.0.1")
+        fs-port (get env "FS_PORT" 8021)
+        fs-pass (get env "FS_PASS" "ClueCon")]
+    (let [conn (connect :host fs-host
+                        :port fs-port
+                        :password fs-pass)]
 
-    ;; Send a simple 'status' api command.
-    (is (= (select-keys (req-api conn "status") [:ok])
-           {:ok true}))
-
-    ;; Turn on a beacon when a background job is complete.
-    (let [beacon (promise)
-          resp (req-bgapi conn
-                          (fn [conn event]
-                            (deliver beacon true))
-                          "status")]
-      (is (= (select-keys resp [:ok])
+      ;; Send a simple 'status' api command.
+      (is (= (select-keys (req-api conn "status") [:ok])
              {:ok true}))
 
-      ;; 500 milliseconds should be enough for the job to complete.
-      (is (= (deref beacon 500 false)
-             true)))
+      ;; Turn on a beacon when a background job is complete.
+      (let [beacon (promise)
+            resp   (req-bgapi conn
+                              (fn [conn event]
+                                (deliver beacon true))
+                              "status")]
+        (is (= (select-keys resp [:ok])
+               {:ok true}))
 
-    ;; Catch background_job with a generalized handler.
-    (let [beacon (promise)]
-      ;; Reset event handlers.
-      (reset! (conn :event-handlers) {})
+        ;; 500 milliseconds should be enough for the job to complete.
+        (is (= (deref beacon 500 false)
+               true)))
 
-      ;; Bind a general handler for bgjob.
-      (bind-event conn
-                  (fn [conn event]
-                    (deliver beacon true))
-                  :event-name "BACKGROUND_JOB")
+      ;; Catch background_job with a generalized handler.
+      (let [beacon (promise)]
+        ;; Reset event handlers.
+        (reset! (conn :event-handlers) {})
 
-      ;; Listen for bgjob event.
-      (req conn ["event BACKGROUND_JOB"] {} nil)
-      ;; Make a bgapi request.
-      (req conn ["bgapi" "status"] {} nil)
-      ;; Wait for our beacon to light-up.
-      (is (= (deref beacon 500 false)
-             true)))
+        ;; Bind a general handler for bgjob.
+        (bind-event conn
+                    (fn [conn event]
+                      (deliver beacon true))
+                    :event-name "BACKGROUND_JOB")
 
-    ;; Catch background_job with a specific handler,
-    ;; even when a general and also a catch-all-stray
-    ;; handler exists.
-    (let [beacon (promise)]
-      ;; Reset event handlers.
-      (reset! (conn :event-handlers) {})
+        ;; Listen for bgjob event.
+        (req conn ["event BACKGROUND_JOB"] {} nil)
+        ;; Make a bgapi request.
+        (req conn ["bgapi" "status"] {} nil)
+        ;; Wait for our beacon to light-up.
+        (is (= (deref beacon 500 false)
+               true)))
 
-      ;; Bind a catch-all-stray handler.
-      (bind-event conn
-                  (fn [conn event]
-                    (deliver beacon :catch-all-stray)))
+      ;; Catch background_job with a specific handler,
+      ;; even when a general and also a catch-all-stray
+      ;; handler exists.
+      (let [beacon (promise)]
+        ;; Reset event handlers.
+        (reset! (conn :event-handlers) {})
 
-      ;; Bind a general handler for bgjob.
-      (bind-event conn
-                  (fn [conn event]
-                    (deliver beacon :general))
-                  :event-name "BACKGROUND_JOB")
+        ;; Bind a catch-all-stray handler.
+        (bind-event conn
+                    (fn [conn event]
+                      (deliver beacon :catch-all-stray)))
 
-      ;; Bind a specific bgjob handler.
-      (bind-event conn
-                  (fn [conn event]
-                    (deliver beacon :specific))
-                  :event-name "BACKGROUND_JOB"
-                  :job-uuid "foobar")
+        ;; Bind a general handler for bgjob.
+        (bind-event conn
+                    (fn [conn event]
+                      (deliver beacon :general))
+                    :event-name "BACKGROUND_JOB")
 
-      ;; Listen for bgjob event.
-      (req conn ["event BACKGROUND_JOB"] {} nil)
-      ;; Make a bgapi request.
-      (req conn ["bgapi" "status"] {:job-uuid "foobar"} nil)
-      ;; Wait for our beacon to light-up.
-      (is (= (deref beacon 500 false)
-             :specific)))))
+        ;; Bind a specific bgjob handler.
+        (bind-event conn
+                    (fn [conn event]
+                      (deliver beacon :specific))
+                    :event-name "BACKGROUND_JOB"
+                    :job-uuid "foobar")
+
+        ;; Listen for bgjob event.
+        (req conn ["event BACKGROUND_JOB"] {} nil)
+        ;; Make a bgapi request.
+        (req conn ["bgapi" "status"] {:job-uuid "foobar"} nil)
+        ;; Wait for our beacon to light-up.
+        (is (= (deref beacon 500 false)
+               :specific))))))
