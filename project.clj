0ddbce08caab3a892099d7770036fbfd35a675d3
Titon Barua <titon@vimmaniac.com>
2021-01-27 21:29:27 +0600

    change: Bumped version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject freeswitch-clj "0.2.3"
+(defproject freeswitch-clj "0.3.0-SNAPSHOT"
   :description "A Clojure interface to freeswitch event socket."
   :url "https://github.com/titonbarua/freeswitch-clj"
   :license {:name "MIT Public License"
