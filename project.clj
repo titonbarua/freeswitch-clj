d16009d778659791ccf953d3d78b64e5081e5d7d
Titon Barua <titanix88@gmail.com>
2024-06-18 23:39:45 -0400

    Upgraded dependencies.
    - Upgraded aleph only works with clojure >= 1.10.x

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "https://github.com/titonbarua/freeswitch-clj"
   :license {:name "MIT Public License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/core.async "0.4.500"]
-                 [org.clojure/tools.logging "1.1.0"]
-                 [org.slf4j/slf4j-simple "1.7.30"]
+  :dependencies [[org.clojure/clojure "1.11.3" :scope "provided"]
+                 [org.clojure/core.async "1.6.681"]
+                 [org.clojure/tools.logging "1.3.0"]
+                 [org.slf4j/slf4j-simple "2.0.13"]
                  [danlentz/clj-uuid "0.1.9"]
-                 [metosin/jsonista "0.3.1"]
-                 [aleph "0.4.6"]
-                 #_[alpeh "0.4.7-alpha7"]]
-  :profiles {:test {:dependencies [[digest "1.4.9"]]}
+                 [metosin/jsonista "0.3.8"]
+                 [aleph "0.8.0"]]
+  :plugins [[lein-ancient "1.0.0-RC3"]]
+  :profiles {:test {:dependencies [[digest "1.4.10"]]}
              :doc-gen {:plugins [[lein-codox "0.10.3"]]
                        :codox {:metadata {:doc/format :markdown}
                                :namespaces [freeswitch-clj.core]
